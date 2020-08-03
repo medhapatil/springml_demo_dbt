4497da6517c72cb747d69fff505878d22272f483
Medha Patil <medhap3@illinois.edu>
2020-08-03 10:55:16 -0400

    dockerfile edit:1.0

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
-FROM fishtownanalytics/dbt:0.18.0b2
-CMD ["run"]
+FROM fishtownanalytics/dbt:0.17.0
+# RUN cd ../Users/medhapatil/springml/springml_demo_dbt
+# RUN apt install -y git
+# RUN git clone 
+COPY profiles.yml /home/dbt_user/.dbt/
+COPY . /dockertest
+WORKDIR /dockertest
+RUN echo $(ls)
+# RUN dbt init springml_demo_dbt_project
+# RUN mv profiles.yml  /home/dbt_user/.dbt/
+RUN dbt run
--- target/run/springml_demo_dbt_project/models/applicant/gender_count.sql
+++ target/run/springml_demo_dbt_project/models/applicant/gender_count.sql
@@ -7,8 +7,8 @@
   as (
     select
     CURRENT_DATE() as Ingestion_date, 
-    countif(source.Gender is not null and source.Gender = "F") as Female_num_count,
-    countif(source.Gender is not null and source.Gender = "M") as Male_num_count,
+    countif(source.Gender is not null and source.Gender = "F") as Female_count,
+    countif(source.Gender is not null and source.Gender = "M") as Male_count,
 from 
     `prismatic-cider-279015`.`springml_demo_dataset`.`applicant_data` as source
   );
